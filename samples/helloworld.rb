16336b801e35ec1fc63bcedf24d5c4564dfab81d
Sebastian Staudt <koraktor@gmail.com>
2009-10-20 15:36:39 +0200

    Added sample for throbber to HelloWorld sample
    Additionally fixed a typo

--- samples/helloworld.rb
+++ samples/helloworld.rb
@@ -6,7 +6,7 @@ require 'rubikon'
 # A simple Hello World application
 class HelloWorld < Rubikon::Application
 
-  # Don't add double-dashed to options automatically
+  # Don't add double-dashes to options automatically
   set :dashed_options, false
 
   # Greet the whole world per default
@@ -20,6 +20,13 @@ class HelloWorld < Rubikon::Application
   action '-i' do
     name = input 'Please enter your name'
     greet name
+  end
+
+  # Sleep for 5 seconds while displaying a throbber
+  action '--throbber' do
+    throbber do
+      sleep 5
+    end
   end
 
   # A standard Ruby class method for greeting
