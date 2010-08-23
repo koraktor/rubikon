8294f0e7143036e1d20fb492cef35da5dacf9621
Sebastian Staudt <koraktor@gmail.com>
2010-08-23 16:06:40 +0200

    Updated Command tests

--- test/command_tests.rb
+++ test/command_tests.rb
@@ -9,6 +9,8 @@ class DummyApp < Application::Base
 
   set :autorun, false
 
+  attr_accessor :external_command_run
+
 end
 
 class CommandTests < Test::Unit::TestCase
@@ -17,17 +19,25 @@ class CommandTests < Test::Unit::TestCase
 
     setup do
       @app = DummyApp.instance
+      @app.instance_eval do
+        @path = File.dirname(__FILE__)
+      end
+    end
+
+    should 'be a Parameter' do
+      assert Command.included_modules.include? Parameter
+      assert Command.new(@app, :command){}.is_a? Parameter
     end
 
     should 'raise an exception when no appliation is given' do
       assert_raise ArgumentError do
-        Command.new nil, 'command'
+        Command.new nil, :command
       end
     end
 
     should 'raise an exception when no code block is given' do
       assert_raise BlockMissingError do
-        Command.new @app, 'command'
+        Command.new @app, :command
       end
     end
 
@@ -40,6 +50,32 @@ class CommandTests < Test::Unit::TestCase
         assert_equal name, command.name
         assert_equal description, command.description
       end
+    end
+
+    should 'correctly parse given parameters' do
+      command = Command.new @app, :command do end
+      flag = Flag.new(:test)
+      command << flag
+      assert_raise UnknownParameterError do
+        command.run(*%w{--test --unknown})
+      end
+      assert flag.active?
+    end
+
+    should 'run the code supplied inside its block' do
+      block_run = false
+      command = Command.new @app, :command do
+        block_run = true
+      end
+      command.run
+      assert block_run
+    end
+
+    should 'run external code if no block is given' do
+      @app.external_command_run = false
+      command = Command.new @app, :external_command
+      command.run
+      assert @app.external_command_run
     end
 
   end
---
+++ test/commands/external_command.rb
@@ -0,0 +1,1 @@
+@external_command_run = true
