480f6c8d9b4a5684e0938b80d590128008834c0a
Sebastian Staudt <koraktor@gmail.com>
2010-10-16 13:55:05 +0200

    Version bump to 0.4.0

--- lib/rubikon.rb
+++ lib/rubikon.rb
@@ -10,12 +10,20 @@ require 'core_ext/object'
 require 'core_ext/string'
 require 'rubikon/application/base'
 
-# A namespace module for all Rubikon related code
+# Rubikon is a simple to use, yet powerful Ruby framework for building
+# console-based applications. Rubikon aims to provide an easy to write and easy
+# to read domain-specific language (DSL) to speed up development of
+# command-line applications. With Rubikon it's a breeze to implement
+# applications with only few options as well as more complex programs like
+# RubyGems, Homebrew or even Git.
+#
+# This is the namespace module for all Rubikon related code.
 #
 # @author Sebastian Staudt
 # @since 0.1.0
 module Rubikon
 
-  VERSION = '0.3.0'
+  # This is the current version of the Rubikon gem
+  VERSION = '0.4.0'
 
 end
