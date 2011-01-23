802cdd6d9507783cf30835d77eb7cb832a8bce7e
Sebastian Staudt <koraktor@gmail.com>
2011-01-23 09:33:31 +0100

    Externalized VERSION for ore

--- lib/rubikon.rb
+++ lib/rubikon.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2009-2010, Sebastian Staudt
+# Copyright (c) 2009-2011, Sebastian Staudt
 
 libdir = File.dirname(__FILE__)
 $:.unshift(libdir) unless $:.include?(libdir)
@@ -10,6 +10,7 @@ require 'core_ext/enumerable'
 require 'core_ext/object'
 require 'core_ext/string'
 require 'rubikon/application/base'
+require 'rubikon/version'
 
 # Rubikon is a simple to use, yet powerful Ruby framework for building
 # console-based applications. Rubikon aims to provide an easy to write and easy
@@ -23,8 +24,4 @@ require 'rubikon/application/base'
 # @author Sebastian Staudt
 # @since 0.1.0
 module Rubikon
-
-  # This is the current version of the Rubikon gem
-  VERSION = '0.5.3'
-
 end
---
+++ lib/rubikon/version.rb
@@ -0,0 +1,11 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2011, Sebastian Staudt
+
+module Rubikon
+
+  # This is the current version of the Rubikon gem
+  VERSION = '0.5.3'
+
+end
