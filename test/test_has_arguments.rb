03734f15f91c9be28f8e2de22da70f08826c2a08
Sebastian Staudt <koraktor@gmail.com>
2011-01-23 09:13:08 +0100

    More tests for HasArguments

--- test/test_has_arguments.rb
+++ test/test_has_arguments.rb
@@ -10,7 +10,8 @@ class HasArg
   include HasArguments
 
   attr_reader :arg_names, :arg_values
-  public :<<, :active!, :arg_count, :args_full?, :check_args, :more_args?
+  public :<<, :active!, :arg_count, :args_full?, :check_args, :more_args?,
+         :reset
 
   def initialize(*options)
      super DummyApp.instance, 'dummy', *options
@@ -42,8 +43,107 @@ class TestHasArguments < Test::Unit::TestCase
       assert_equal ({}), has_arg.arg_values
     end
 
+    should 'allow a argument validation for normal arguments' do
+      has_arg = HasArg.new(:arg1, :arg2 => ['string', /regexp/, :numeric])
+      assert_equal 2..2, has_arg.arg_count
+      assert_equal [:arg1, :arg2], has_arg.arg_names
+      assert_equal 1, has_arg.arg_values.size
+      assert_equal /^(?-mix:string|(?-mix:regexp)|(?-mix:-?[0-9]+))$/, has_arg.arg_values[:arg2]
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << 'string'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << 'regexp'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << '123'
+        has_arg.check_args
+      end
+    end
+
+    should 'allow a argument validation for optional arguments' do
+      has_arg = HasArg.new(:arg1, :arg2 => [:optional, 'string', /regexp/, :numeric])
+      assert_equal 1..2, has_arg.arg_count
+      assert_equal [:arg1, :arg2], has_arg.arg_names
+      assert_equal 1, has_arg.arg_values.size
+      assert_equal /^(?-mix:string|(?-mix:regexp)|(?-mix:-?[0-9]+))$/, has_arg.arg_values[:arg2]
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << 'string'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << 'regexp'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << '123'
+        has_arg.check_args
+      end
+    end
+
+    should 'allow a argument validation for remainder arguments' do
+      has_arg = HasArg.new(:arg1, :arg2 => [:remainder, 'string', /regexp/, :numeric])
+      assert_equal 2..-1, has_arg.arg_count
+      assert_equal [:arg1, :arg2], has_arg.arg_names
+      assert_equal 1, has_arg.arg_values.size
+      assert_equal /^(?-mix:string|(?-mix:regexp)|(?-mix:-?[0-9]+))$/, has_arg.arg_values[:arg2]
+
+      assert_raise MissingArgumentError do
+        has_arg << 'test'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_raise UnexpectedArgumentError do
+        has_arg << 'test'
+        has_arg << 'test'
+        has_arg.check_args
+      end
+
+      has_arg.reset
+
+      assert_nothing_raised do
+        has_arg << 'test'
+        has_arg << 'string'
+        has_arg << 'regexp'
+        has_arg << '123'
+        has_arg.check_args
+      end
+    end
+
     should 'only have required arguments if argument count is > 0' do
       has_arg = HasArg.new 2
+      assert_equal 2..2, has_arg.arg_count
       assert !has_arg.args_full?
       assert has_arg.more_args?
       has_arg << 'argument'
@@ -63,6 +163,7 @@ class TestHasArguments < Test::Unit::TestCase
 
     should 'have required and optional arguments if argument count is < 0' do
       has_arg = HasArg.new -1
+      assert_equal 1..-1, has_arg.arg_count
       assert !has_arg.args_full?
       assert has_arg.more_args?
       assert_raise MissingArgumentError do
@@ -76,6 +177,7 @@ class TestHasArguments < Test::Unit::TestCase
 
     should 'only have optional arguments if argument count is 0' do
       has_arg = HasArg.new 0
+      assert_equal 0..-1, has_arg.arg_count
       assert has_arg.args_full?
       assert has_arg.more_args?
       has_arg << 'argument'
@@ -84,6 +186,7 @@ class TestHasArguments < Test::Unit::TestCase
 
     should 'provide named arguments' do
       has_arg = HasArg.new :named
+      assert_equal 1..1, has_arg.arg_count
       has_arg << 'argument'
       assert_equal 'argument', has_arg[:named]
       assert_equal 'argument', has_arg.named
@@ -94,6 +197,7 @@ class TestHasArguments < Test::Unit::TestCase
 
     should 'provide optional named arguments' do
       has_arg = HasArg.new :required, :optional => :optional
+      assert_equal 1..2, has_arg.arg_count
       has_arg << 'argument'
       assert has_arg.args_full?
       assert has_arg.more_args?
@@ -105,6 +209,22 @@ class TestHasArguments < Test::Unit::TestCase
 
     should 'provide named arguments taking all remainding arguments' do
       has_arg = HasArg.new :required, :remainder => :remainder
+      assert_equal 2..-1, has_arg.arg_count
+      has_arg << 'argument'
+      assert !has_arg.args_full?
+      assert has_arg.more_args?
+      has_arg << 'argument'
+      assert has_arg.args_full?
+      assert has_arg.more_args?
+      has_arg << 'argument'
+      assert has_arg.more_args?
+      assert_equal 'argument', has_arg.required
+      assert_equal %w{argument argument}, has_arg.remainder
+    end
+
+    should 'provide named arguments optionally taking all remainding arguments' do
+      has_arg = HasArg.new :required, :remainder => [:optional, :remainder]
+      assert_equal 1..-1, has_arg.arg_count
       has_arg << 'argument'
       assert has_arg.args_full?
       assert has_arg.more_args?
